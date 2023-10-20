2c1dd7d0860776f3b086df70225f87b8215f6bf6
emilu09 <128389528+emilu09@users.noreply.github.com>
2023-10-20 11:07:07 -0300

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,14 @@
+functiontocasom(seletorAudio){
+constelemento=document.querySelector(selectorAudio);
+if(elements&&elementolocalName==='audio'){
+elementoplay();
+}
+else{
+consolelog('Elemento não encontrado ou seletor inválido');
+}
+  }
+const listaDeTeclas=documentquerySelector All('tecla')
+for(let contador=O;contador<listaDeTeclas lenght;contador++){
+const tecla=listaDeTeclas[contador]
+const instrumento=classlist[1];
+const Audio='#som_${instrumento}';
